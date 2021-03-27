ef49ecd1ca3a5948f05abcc14f1cf99adb91c04f
Aminu Muhammad <elminu80@gmail.com>
2021-03-27 15:31:56 +0100

    maghassan
    Changed to single line.

--- script/passgenSript.js
+++ script/passgenSript.js
@@ -1,8 +1,8 @@
 var passgen =   angular.module('passgen',   []);
 
 passgen.controller('ctrl1', function($scope)    {
-    $scope.randomNum1   =   Math.floor((Math.random()   *   10) +   1);
-    $scope.randomNum2   =   Math.floor((Math.random()   *   10) +   1);
-    $scope.randomNum3   =   Math.floor((Math.random()   *   10) +   1);
-    $scope.randomNum4   =   Math.floor((Math.random()   *   10) +   1);
+    $scope.randomNum1   =   Math.floor((Math.random()   *   9) +   1);
+    $scope.randomNum2   =   Math.floor((Math.random()   *   9) +   1);
+    $scope.randomNum3   =   Math.floor((Math.random()   *   9) +   1);
+    $scope.randomNum4   =   Math.floor((Math.random()   *   9) +   1);
 })
